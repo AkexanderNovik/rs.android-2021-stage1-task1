bfb17b89ab916e5f42702ae1c45c96ac75b8d328
AkexanderNovik <hyper_alex@mail.ru>
2021-05-25 23:39:49 +0300

    feat: implement HappyArray

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,22 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val array = mutableListOf<Int>()
+
+        return if (sadArray.isNotEmpty() && sadArray.size > 2) {
+
+            array.add(sadArray[0])
+            val indexOfTheLastElement = sadArray.size - 1
+
+            for (i in 1 until indexOfTheLastElement) {
+                if (sadArray[i - 1] + sadArray[i + 1] > sadArray[i])
+                    array.add(sadArray[i])
+            }
+
+            array.add(sadArray[indexOfTheLastElement])
+
+            array.toIntArray()
+        }else array.toIntArray()
     }
+
 }
