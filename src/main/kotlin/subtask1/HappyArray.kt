4c3752296fc7a47de6858db14f3b2442df2be772
AkexanderNovik <hyper_alex@mail.ru>
2021-05-26 18:33:38 +0300

    feat: refactoring

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -9,17 +9,22 @@ class HappyArray {
         return if (sadArray.isNotEmpty() && sadArray.size > 2) {
 
             array.add(sadArray[0])
-            val indexOfTheLastElement = sadArray.size - 1
+            val indexOfTheLastElement = sadArray.lastIndex
+            var arraySize = array.size
 
             for (i in 1 until indexOfTheLastElement) {
-                if (sadArray[i - 1] + sadArray[i + 1] > sadArray[i])
+                if (sadArray[i - 1] + sadArray[i + 1] > sadArray[i]) {
                     array.add(sadArray[i])
-                var t = array.size
-                if (t > 2 && array[t - 3] + array[t - 1] < array[t - 2]) {
-                    array.removeAt(t - 2)
+                    arraySize += 1
+                }
+                if (arraySize > 2
+                    && array[arraySize - 3] + array.last() < array[arraySize - 2]
+                ) {
+                    array.removeAt(arraySize - 2)
+                    arraySize -= 1
                 }
             }
-            array.add(sadArray[indexOfTheLastElement])
+            array.add(sadArray.last())
             array.toIntArray()
         } else array.toIntArray()
     }
